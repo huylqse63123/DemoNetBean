1049d8d775c315c11bf541bc341a5e4540085bb1
Le Huy <cwalklhuy@gmail.com>
2018-11-18 15:16:34 +0700

    test lan 2

--- main.java
+++ main.java
@@ -13,5 +13,6 @@ public class main {
    public static void main(String[] args) {
       System.out.println("Hello mortor");
       System.out.println("Hello ver 2");
+      System.out.println("Hello ver 3");
    }
 }
